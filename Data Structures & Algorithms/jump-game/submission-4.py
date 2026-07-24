15c55fe1a4d34bcdf6ec6686bdb0cda3ed854246
AsaphUclouvain <asaph.nzongan@student.uclouvain.be>
2026-07-24 05:49:53 +0200

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        def dfs(cur: int, memo: List[int]):
+            if cur >= len(nums):
+                return False
+            if cur in memo:
+                return memo[cur]
+            if cur == len(nums) - 1:
+                memo[cur] = True
+                return True
+            end = min(len(nums) - 1, cur + nums[cur])
+            for i in range(cur + 1, end + 1):
+                if dfs(i, memo):
+                    memo[cur] = True
+                    return True
+            memo[cur] = False
+            return False
+        return dfs(0, {})
